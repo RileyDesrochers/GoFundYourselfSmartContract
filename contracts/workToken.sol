7e434077f8e732b42230d72adc24d6c47caca597
tippi fifestarr <tippi@boober.org>
2022-05-26 23:34:18 -0500

    didn't do a code

--- contracts/workToken.sol
+++ contracts/workToken.sol
@@ -10,7 +10,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 import "hardhat/console.sol";
 
-struct ProjectCreater { 
+struct ProjectCreator { 
    uint256 totalJobCost;
    uint16 jobs;
    uint256 jobTimeLimit;
@@ -30,10 +30,10 @@ contract workToken is Ownable, ERC721URIStorage, KeeperCompatibleInterface{
     enum tokenStatus{unFulfilled, fulfilled, expired, flagged}
 
     mapping(uint256 => tokenStatus) public tokenIdToStatus;
-    mapping(uint256 => address) public tokenIdToProjectCreater;
+    mapping(uint256 => address) public tokenIdToProjectCreator;
     mapping(uint256 => uint256) public tokenIdToExpiryTime;
 
-    mapping(address => ProjectCreater) public ProjectCreaters;
+    mapping(address => ProjectCreator) public ProjectCreators;
 
     constructor() ERC721("WorkToken","WRK"){
         tokenCounter = 0;
@@ -45,51 +45,51 @@ contract workToken is Ownable, ERC721URIStorage, KeeperCompatibleInterface{
         stableCoin = IERC20(_stableCoinAddress);
     }
     
-    function mintNFT(address ProjectCreaterAddress) public returns (uint256){//
-        require(ProjectCreaters[ProjectCreaterAddress].jobsMinted <= ProjectCreaters[ProjectCreaterAddress].jobs);
+    function mintNFT(address ProjectCreatorAddress) public returns (uint256){//
+        require(ProjectCreators[ProjectCreatorAddress].jobsMinted <= ProjectCreators[ProjectCreatorAddress].jobs);
         tokenIdToStatus[tokenCounter] = tokenStatus(0);
-        tokenIdToProjectCreater[tokenCounter] = ProjectCreaterAddress;
-        tokenIdToExpiryTime[tokenCounter] = block.timestamp + ProjectCreaters[ProjectCreaterAddress].jobTimeLimit;
+        tokenIdToProjectCreator[tokenCounter] = ProjectCreatorAddress;
+        tokenIdToExpiryTime[tokenCounter] = block.timestamp + ProjectCreators[ProjectCreatorAddress].jobTimeLimit;
 
-        ProjectCreaters[ProjectCreaterAddress].jobsIssued.push(tokenCounter);
-        ProjectCreaters[ProjectCreaterAddress].jobsMinted++;
+        ProjectCreators[ProjectCreatorAddress].jobsIssued.push(tokenCounter);
+        ProjectCreators[ProjectCreatorAddress].jobsMinted++;
 
-        uint256 jobCost = 10 ** 6 * ProjectCreaters[ProjectCreaterAddress].totalJobCost / ProjectCreaters[ProjectCreaterAddress].jobs;
+        uint256 jobCost = 10 ** 6 * ProjectCreators[ProjectCreatorAddress].totalJobCost / ProjectCreators[ProjectCreatorAddress].jobs;
 
-        stableCoin.safeTransferFrom(msg.sender, ProjectCreaterAddress, jobCost);
+        stableCoin.safeTransferFrom(msg.sender, ProjectCreatorAddress, jobCost);
 
         _safeMint(msg.sender, tokenCounter);
-        _setTokenURI(tokenCounter, ProjectCreaters[ProjectCreaterAddress].tokenURI);
+        _setTokenURI(tokenCounter, ProjectCreators[ProjectCreatorAddress].tokenURI);
         tokenCounter++;
         return tokenCounter - 1;
     }
 
-    function newProjectCreater(uint16 jobs, uint256 jobTimeLimit, string memory tokenURI) public {
-        require(ProjectCreaters[msg.sender].totalJobCost == 0);
-        ProjectCreaters[msg.sender].totalJobCost = startTotalJobCost;
-        projectCreaterChangeJob(jobs, jobTimeLimit, tokenURI);
+    function newProjectCreator(uint16 jobs, uint256 jobTimeLimit, string memory tokenURI) public {
+        require(ProjectCreators[msg.sender].totalJobCost == 0);
+        ProjectCreators[msg.sender].totalJobCost = startTotalJobCost;
+        ProjectCreatorChangeJob(jobs, jobTimeLimit, tokenURI);
     }
 
-    function projectCreaterChangeJob(uint16 jobs, uint256 jobTimeLimit, string memory tokenURI) public {
+    function ProjectCreatorChangeJob(uint16 jobs, uint256 jobTimeLimit, string memory tokenURI) public {
         require(jobs <= jobLimit && jobs > 0);
-        require(ProjectCreaters[msg.sender].totalJobCost != 0);
-        ProjectCreaters[msg.sender].jobs = jobs;
-        ProjectCreaters[msg.sender].jobTimeLimit = jobTimeLimit;
-        ProjectCreaters[msg.sender].tokenURI = tokenURI;
-        ProjectCreaters[msg.sender].jobsCompleted = 0;//jobsMinted
-        ProjectCreaters[msg.sender].jobsMinted = 0;
+        require(ProjectCreators[msg.sender].totalJobCost != 0);
+        ProjectCreators[msg.sender].jobs = jobs;
+        ProjectCreators[msg.sender].jobTimeLimit = jobTimeLimit;
+        ProjectCreators[msg.sender].tokenURI = tokenURI;
+        ProjectCreators[msg.sender].jobsCompleted = 0;//jobsMinted
+        ProjectCreators[msg.sender].jobsMinted = 0;
     }
 
-    function getProjectCreater(address projectCreater) public view returns (ProjectCreater memory) {
-        return ProjectCreaters[projectCreater];
+    function getProjectCreator(address ProjectCreator) public view returns (ProjectCreator memory) {
+        return ProjectCreators[ProjectCreator];
     }
 
     function completeJob(uint256 jobIndex) public { // add modifyer
-        address projectCreater = tokenIdToProjectCreater[jobIndex];
+        address ProjectCreator = tokenIdToProjectCreator[jobIndex];
         
         tokenIdToStatus[jobIndex] = tokenStatus(1);
-        if(keccak256(abi.encodePacked(ProjectCreaters[projectCreater].tokenURI)) == keccak256(abi.encodePacked(tokenURI(jobIndex)))) {
-            ProjectCreaters[projectCreater].jobsCompleted++;
+        if(keccak256(abi.encodePacked(ProjectCreators[ProjectCreator].tokenURI)) == keccak256(abi.encodePacked(tokenURI(jobIndex)))) {
+            ProjectCreators[ProjectCreator].jobsCompleted++;
         }
     }
 
@@ -106,10 +106,10 @@ contract workToken is Ownable, ERC721URIStorage, KeeperCompatibleInterface{
         tokenIdToStatus[jobIndex] = tokenStatus(3);
     }
     
-    function upgradeProjectCreater() public {
-        if(ProjectCreaters[msg.sender].jobsCompleted == ProjectCreaters[msg.sender].jobs){
-            ProjectCreaters[msg.sender].totalJobCost = ProjectCreaters[msg.sender].totalJobCost * 3 / 2;
-            ProjectCreaters[msg.sender].jobsCompleted = 0;
+    function upgradeProjectCreator() public {
+        if(ProjectCreators[msg.sender].jobsCompleted == ProjectCreators[msg.sender].jobs){
+            ProjectCreators[msg.sender].totalJobCost = ProjectCreators[msg.sender].totalJobCost * 3 / 2;
+            ProjectCreators[msg.sender].jobsCompleted = 0;
         }
     }
 
